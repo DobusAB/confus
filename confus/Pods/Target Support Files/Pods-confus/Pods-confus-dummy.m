9b1dc78ae24c7ea0da399233fc1c127ddb70ba6e
Christopher Dobus <christopherdobus@Christophers-MacBook-Air-2.local>
2016-03-04 18:34:53 +0100

    Installerat estimote SDK.

---
+++ confus/Pods/Target Support Files/Pods-confus/Pods-confus-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_confus : NSObject
+@end
+@implementation PodsDummy_Pods_confus
+@end
---
+++ confus/confus/ObjCBridge.h
@@ -0,0 +1,15 @@
+//
+//  ObjCBridge.h
+//  confus
+//
+//  Created by Christopher Dobus on 2016-03-04.
+//  Copyright © 2016 Dobus. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+
+#endif /* ObjCBridge_h */
+
+#import <EstimoteSDK/EstimoteSDK.h>
